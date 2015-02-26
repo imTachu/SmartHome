044c45866d5fe6c6391cbfdd10fface439272489
Tachu <tachu.salamanca@gmail.com>
2015-02-26 18:40:55 -0500

    WAT-36, avance

--- watchapp/urls.py
+++ watchapp/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth.views import login, logout
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^home/$', views.home, name='home'),
     url(r'^constructora_home/$', views.constructora_home, name='constructora_home'),
     url(r'^users_home/$', views.users_home, name='users_home'),
     url(r'^login/$', login, {'template_name': 'watchapp/login.html', }, name="login"),
@@ -15,4 +14,6 @@ urlpatterns = patterns('',
 	url(r'^accounts/login$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', logout, {'template_name': 'watchapp/login.html', }, name="logout"),
 	url('', include('django.contrib.auth.urls')),
+	
+	url(r'^get_property_info_by_name/', views.get_property_info_by_name, name='get_property_info_by_name'),
 )
--- watchapp/views.py
+++ watchapp/views.py
@@ -7,21 +7,27 @@ from django.contrib.auth.models import User, Group
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
- 
+from django.core.exceptions import ObjectDoesNotExist
 from forms import SignUpForm
 from watchapp.models import ConstructorCompany, Property, UserProfile, Sensor, Event
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the watchapp index.")
-	
-@login_required()
-def home(request):
-    return render_to_response('watchapp/home.html', {'user': request.user}, context_instance=RequestContext(request))
 
-def main(request):
-    return render_to_response('watchapp/main.html', {}, context_instance=RequestContext(request))
+####################### Vistas index o para autenticacion #######################
+
+def index(request):
+	"""
+	Esta vista es la pagina principal de WatchApp.
+		@param request
+		@author Lorena Salamanca
+	"""
+	return HttpResponse("Hello, world. You're at the watchapp index.")
 
 def signup(request):
+    """
+    Esta funcion contiene el formulario de creacion de usuarios para WatchApp
+    	@param request
+    	@author Lorena Salamanca
+    """
     if request.method == 'POST':  # If the form has been submitted...
         form = SignUpForm(request.POST)  # A form bound to the POST data
         if form.is_valid():  # All validation rules pass
@@ -52,22 +58,67 @@ def signup(request):
     return render_to_response('watchapp/signup.html', data, context_instance=RequestContext(request))
 
 def login_success(request):
+    """
+    Esta funcion redirecciona al home de constructoras o de usuarios (propietarios / residentes)
+    dependiendo del Group al que pertenezca el usuario autenticado.
+    	@param request
+    	@author Lorena Salamanca
+    """
     if request.user.groups.filter(name="constructoras").exists():
-        # user is an admin
         return HttpResponseRedirect(reverse('watchapp:constructora_home'))
     elif request.user.groups.filter(name="usuarios").exists():
-        return HttpResponseRedirect(reverse('watchapp:users_home'))
+		return HttpResponseRedirect(reverse('watchapp:users_home'))
 
-@login_required()
-@user_passes_test(lambda u: u.groups.filter(name='constructoras').exists(), login_url='/watchapp/login/')
-def constructora_home(request):
-	return HttpResponse("Usuario constructora autenticado")
+####################### Vistas para propietarios / residentes #######################
 
 @login_required()
 @user_passes_test(lambda u: u.groups.filter(name='usuarios').exists(), login_url='/watchapp/login/')
 def users_home(request):
-	print request.user.username
-	return render(request, 'watchapp/users_home.html',
-    {
-    	"request": request,
+    """
+    Esta funcion tiene el contenido del home de usuarios (propietarios / residentes), 
+    se puede acceder despues de la autenticacion de un usuario que este en el Group usuarios
+    	@param request
+    	@author Lorena Salamanca
+    """
+    if request.method == 'POST':
+        try:
+            selected_property = Property.objects.get(name=request.POST["select_as_resident"])
+        except Property.DoesNotExist:
+            selected_property = Property.objects.get(name=request.POST["select_as_owner"])
+        return render(request, 'watchapp/users_home.html', {
+        "selected_property": selected_property,
+        "request": request,
+        })
+    else:
+        return render(request, 'watchapp/users_home.html', {
+            "request": request,
+        })
+
+@login_required()
+@user_passes_test(lambda u: u.groups.filter(name='usuarios').exists(), login_url='/watchapp/login/')
+def get_property_info_by_name(request):
+    """
+    Esta funcion tiene el contenido del home de usuarios (propietarios / residentes), 
+    se puede acceder despues de la autenticacion de un usuario que este en el Group usuarios
+    	@param request
+    	@author Lorena Salamanca
+    """
+    print request.POST["select_as_resident"]
+    selected_property = Property.objects.get(name=request.POST["select_as_resident"])
+    return render(request, 'watchapp/users_home.html', {
+        "selected_property": selected_property,
+        "request": request,
     })
+
+####################### Vistas para constructoras #######################
+
+@login_required()
+@user_passes_test(lambda u: u.groups.filter(name='constructoras').exists(), login_url='/watchapp/login/')
+def constructora_home(request):
+    """
+    Esta funcion tiene el contenido del home de constructoras, 
+    se puede acceder despues de la autenticacion de un usuario que este en el Group constructoras
+    	@param request
+    	@author Lorena Salamanca
+    """
+    return HttpResponse("Usuario constructora autenticado")
